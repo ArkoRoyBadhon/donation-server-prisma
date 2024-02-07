3813531bf96592145a366859d13dd7dd5f0995a1
Arko <arkoroyb@gmail.com>
2024-02-08 01:14:32 +0600

    project tsconfig file modify

---
+++ dist/app/modules/donation/donation.controller.js
@@ -0,0 +1,108 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.donationController = void 0;
+const http_status_1 = __importDefault(require("http-status"));
+const sendResponse_1 = __importDefault(require("../../../shared/sendResponse"));
+const donation_service_1 = require("./donation.service");
+const catchAsync_1 = __importDefault(require("../../../shared/catchAsync"));
+const createDonation = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const data = req.body;
+    const result = yield donation_service_1.donationService.createDonation(data);
+    (0, sendResponse_1.default)(res, {
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: 'Donation created successfully!',
+        data: result,
+    });
+}));
+const getAllDonation = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { searchTerm } = req.query;
+    const result = yield donation_service_1.donationService.getAllDonation(searchTerm);
+    (0, sendResponse_1.default)(res, {
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: 'All Donation Retrieved successfully!',
+        data: result,
+    });
+}));
+const getSingleDonation = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const result = yield donation_service_1.donationService.getSingleDonation(id);
+    (0, sendResponse_1.default)(res, {
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: 'Donation Retrieved successfully!',
+        data: result,
+    });
+}));
+const updateSingleDonation = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const payload = req.body;
+    const result = yield donation_service_1.donationService.updateSingleDonation(id, payload);
+    (0, sendResponse_1.default)(res, {
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: 'Donation updated successfully!',
+        data: result,
+    });
+}));
+const donationExecute = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const data = req.body;
+    const result = yield donation_service_1.donationService.donationExecute(data);
+    (0, sendResponse_1.default)(res, {
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: 'You successfully Donate!',
+        data: result,
+    });
+}));
+const getAllDonationExecute = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield donation_service_1.donationService.getAllDonationExecute();
+    (0, sendResponse_1.default)(res, {
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: 'All Donation retrieved successfully!',
+        data: result,
+    });
+}));
+const getSingleUserDonationExecute = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const result = yield donation_service_1.donationService.getSingleUserDonationExecute(id);
+    (0, sendResponse_1.default)(res, {
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: 'All Donation retrieved successfully!',
+        data: result,
+    });
+}));
+const donationCalculations = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield donation_service_1.donationService.donationCalculations();
+    (0, sendResponse_1.default)(res, {
+        statusCode: http_status_1.default.OK,
+        success: true,
+        message: 'All Donation Calculations retrieved successfully!',
+        data: result,
+    });
+}));
+exports.donationController = {
+    createDonation,
+    getAllDonation,
+    getSingleDonation,
+    updateSingleDonation,
+    donationExecute,
+    getAllDonationExecute,
+    getSingleUserDonationExecute,
+    donationCalculations,
+};
---
+++ dist/app/modules/donation/donation.routes.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.donationRoutes = void 0;
+const express_1 = __importDefault(require("express"));
+const donation_controller_1 = require("./donation.controller");
+const auth_1 = __importDefault(require("../../middlewares/auth"));
+const users_1 = require("../../../enums/users");
+const router = express_1.default.Router();
+router.post('/donation/create', (0, auth_1.default)(users_1.ENUM_USER_ROLE.ADMIN), donation_controller_1.donationController.createDonation);
+router.get('/donation/get-all', donation_controller_1.donationController.getAllDonation);
+router.get('/donation/get-single/:id', donation_controller_1.donationController.getSingleDonation);
+router.patch('/donation/update/:id', (0, auth_1.default)(users_1.ENUM_USER_ROLE.ADMIN), donation_controller_1.donationController.updateSingleDonation);
+router.post('/donation/user-donate', (0, auth_1.default)(users_1.ENUM_USER_ROLE.USER, users_1.ENUM_USER_ROLE.ADMIN), donation_controller_1.donationController.donationExecute);
+router.get('/donation/user-donate', (0, auth_1.default)(users_1.ENUM_USER_ROLE.USER, users_1.ENUM_USER_ROLE.ADMIN), donation_controller_1.donationController.getAllDonationExecute);
+router.get('/donation/user-donate/:id', donation_controller_1.donationController.getSingleUserDonationExecute);
+router.get('/donation/calculation', (0, auth_1.default)(users_1.ENUM_USER_ROLE.ADMIN), donation_controller_1.donationController.donationCalculations);
+exports.donationRoutes = router;
